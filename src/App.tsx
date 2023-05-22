228fb2949bcb16e7f82b17da41f0576edec511d2
Adi-Goll <adigollamudi@gmail.com>
2023-05-21 22:33:17 -0400

    title text and globe map with no background

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef, useEffect, useState, createRef } from 'react';
+import Map, { Source, Marker, Layer, GeolocateControl } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './App.css';
+
+<link href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css" rel="stylesheet" />;
 
 function App() {
-  const [count, setCount] = useState(0)
+   const mapContainer = useRef(null);
+   const [lng, setLng] = useState(57.3758576273);
+   const [lat, setLat] = useState(24.45677614934);
+   const [zoom, setZoom] = useState(1);
+   const [viewport, setViewport] = useState({});
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   useEffect(() => {
+      navigator.geolocation.getCurrentPosition((pos) => {
+         setViewport({
+            ...viewport,
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+            zoom: 3.5,
+         });
+      });
+   }, []);
+
+   return (
+      <div id="content-div">
+         <h1>Global Climate News</h1>
+         <h3>Select any country!</h3>
+         <Map
+            mapboxAccessToken="pk.eyJ1IjoiYWdvbGwwMSIsImEiOiJjbGhmNWF4ZWgwMXhjM3FwZGFkYnIzczM0In0.o6-UGqbzNBNrpA-he4s2PA"
+            style={{
+               container: 'map',
+               width: '76vw',
+               height: '60vh',
+               borderRadius: '30px',
+               border: '2px solid blue',
+            }}
+            initialViewState={{
+               latitude: lat,
+               longitude: lng,
+               zoom: 1,
+            }}
+            projection={'globe'}
+            mapStyle="mapbox://styles/mapbox/satellite-v9"
+         >
+            <GeolocateControl
+               positionOptions={{ enableHighAccuracy: true }}
+               trackUserLocation={true}
+            />
+         </Map>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+   );
 }
 
-export default App
+export default App;
