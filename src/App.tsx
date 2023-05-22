36775dcae86e2da1ff822dcbbd76d1708a3eba50
Adi-Goll <adigollamudi@gmail.com>
2023-05-22 13:45:50 -0400

    globe map with background console logs country that you clicked on by making API request with coordinates

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,80 @@
 import React, { useRef, useEffect, useState, createRef } from 'react';
-import Map, { Source, Marker, Layer, GeolocateControl } from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import Map, {
+   Source,
+   Marker,
+   Layer,
+   GeolocateControl,
+   NavigationControl,
+   FullscreenControl,
+} from 'react-map-gl';
+// import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
-
-<link href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css" rel="stylesheet" />;
+import mapboxgl from 'mapbox-gl';
+import axios from 'axios';
 
 function App() {
    const mapContainer = useRef(null);
-   const [lng, setLng] = useState(57.3758576273);
-   const [lat, setLat] = useState(24.45677614934);
+   const [lng, setLng] = useState(77.209);
+   const [lat, setLat] = useState(28.6139);
    const [zoom, setZoom] = useState(1);
-   const [viewport, setViewport] = useState({});
 
-   useEffect(() => {
-      navigator.geolocation.getCurrentPosition((pos) => {
-         setViewport({
-            ...viewport,
-            latitude: pos.coords.latitude,
-            longitude: pos.coords.longitude,
-            zoom: 3.5,
+   const getCountry = () => {
+      axios
+         .get(
+            `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=8c473e6979884d52a85c6ddcb697b502`,
+         )
+         .then(function (response) {
+            console.log(response.data.results[0].components['ISO_3166-1_alpha-2']);
+         })
+         .catch(function (error) {
+            console.log(error);
          });
-      });
-   }, []);
+   };
 
    return (
-      <div id="content-div">
+      <div>
+         <link
+            href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.14.1/mapbox-gl.css"
+            rel="stylesheet"
+         />
          <h1>Global Climate News</h1>
-         <h3>Select any country!</h3>
+         <h3>Select any country</h3>
+         <pre id="info"></pre>
          <Map
             mapboxAccessToken="pk.eyJ1IjoiYWdvbGwwMSIsImEiOiJjbGhmNWF4ZWgwMXhjM3FwZGFkYnIzczM0In0.o6-UGqbzNBNrpA-he4s2PA"
             style={{
                container: 'map',
-               width: '76vw',
-               height: '60vh',
-               borderRadius: '30px',
-               border: '2px solid blue',
+               width: '80vw',
+               height: '70vh',
+               border: 'solid 2px red',
             }}
             initialViewState={{
                latitude: lat,
                longitude: lng,
-               zoom: 1,
+               zoom: 5,
+            }}
+            onMouseMove={(e) => {
+               document.getElementById('info')!.innerHTML =
+                  // `e.point` is the x, y coordinates of the `mousemove` event
+                  // relative to the top-left corner of the map.
+                  JSON.stringify(e.point) +
+                  '<br />' +
+                  // `e.lngLat` is the longitude, latitude geographical position of the event.
+                  JSON.stringify(e.lngLat.wrap());
+               setLng(Number(JSON.stringify(e.lngLat).split(',')[0].slice(7)));
+               setLat(Number(JSON.stringify(e.lngLat).split(',')[1].slice(6, -1)));
+            }}
+            onClick={() => {
+               console.log('click noticed at');
+               console.log('longitude: ', lng, 'latitude: ', lat);
+               getCountry();
             }}
             projection={'globe'}
-            mapStyle="mapbox://styles/mapbox/satellite-v9"
+            mapStyle="mapbox://styles/mapbox/streets-v12"
          >
-            <GeolocateControl
-               positionOptions={{ enableHighAccuracy: true }}
-               trackUserLocation={true}
-            />
+            <NavigationControl position="top-right" />
+            <Marker longitude={77.209} latitude={28.6139} />
+            <FullscreenControl />
          </Map>
       </div>
    );
